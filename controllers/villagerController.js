94c9df22557a6ddb5b7e1aa02f5e53c1d811d673
kchu01 <chukathy01@gmail.com>
2021-03-02 13:47:17 -0800

    Add Villager Route

--- controllers/villagerController.js
+++ controllers/villagerController.js
@@ -7,9 +7,10 @@ router.get('/', async (req, res) => {
     try {
         const acnhURL = 'https://acnhapi.com/v1/villagers'
         const response = await axios.get(acnhURL)
-        const villagers = response.data.ant00.name
-        const name = Object.values(villagers)[0]
-        // for in loop to grab (currently ant00) the objects
+        const villagers = response.data
+        const name = Object.values(villagers)[0].name['name-USen']
+
+
         console.log(name)
         res.render('./villagers', { name: name })
     } catch (error) {
